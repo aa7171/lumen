ab4b3bdd9eec3d5feef48d58df0e7c4e6fab320c
aa7171 <45379853+aa7171@users.noreply.github.com>
2018-11-29 16:59:09 -0600

    Update ptrarr.c

--- Day1/ptrarr.c
+++ Day1/ptrarr.c
@@ -4,6 +4,10 @@ int main()
 {
         int arr[4] = {1,2,3,4};
         int *iptr;
+
+        printf("Address of 0th index = %p\n", arr);
+        printf("Address of 0th index = %p\n", &arr[0]);
+
         iptr = arr + 2; //pointing to the 3rd element
         printf("Address of 2nd index = %p\n", iptr);
         iptr = &arr[2]; //pointing to the 3rd element
